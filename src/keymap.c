da27d3ad697b5775b31d41c6389f8684babe3bf9
Stefan Adrian Danaita <me@dsa.io>
2019-02-26 11:37:55 +0000

    Version 1.1.1

--- src/keymap.c
+++ src/keymap.c
@@ -7,82 +7,19 @@
 #include "keymap_french.h"
 #include "keymap_spanish.h"
 
-#define LCGS(code) LCTL(LGUI(LSFT(code)))
-#define LCS(code) LCTL(LSFT(code))
-
 enum custom_keycodes {
-  PLACEHOLDER = SAFE_RANGE,      // can always be here
-  EPRM,           
-  RGB_SLD,        
+  PLACEHOLDER = SAFE_RANGE, // can always be here
+  EPRM,
+  RGB_SLD,
   
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_ergodox(
-    // left hand
-    KC_ESC,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_P6,          
-    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           _______,        
-    LT(1,KC_BSPC),  KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           
-    KC_LSFT,        LCTL_T(KC_Z),   KC_X,           KC_C,           KC_V,           KC_B,           _______,        
-    KC_GRV,         _______,        _______,        LT(2,XXXXXXX),  LT(1,XXXXXXX),  
-                                                                                    LCS(KC_TAB),    LCTL(KC_TAB),   
-                                                                                                    KC_HOME,        
-                                                                    KC_SPC,         LGUI_T(XXXXXXX),KC_END,         
-        // right hand
-        KC_P7,          KC_8,           KC_9,           KC_0,           KC_MINS,        KC_EQL,         KC_BSPC,        
-        _______,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,        
-                        KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_QUOT,        
-        KC_COLN,        KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,        KC_RSFT,        
-        KC_LPRN,        KC_RPRN,        KC_LBRC,        KC_RBRC,        KC_HASH,        
-        KC_LALT,        LCTL_T(KC_ESC), 
-        KC_PGUP,        
-        KC_PGDN,        RGUI_T(XXXXXXX),KC_ENT
-    ),
+  [0] = LAYOUT_ergodox(KC_ESCAPE,KC_1,KC_2,KC_3,KC_4,KC_5,KC_KP_6,KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,KC_COPY,LT(1,KC_BSPACE),KC_A,KC_S,KC_D,KC_F,KC_G,KC_LSHIFT,LCTL_T(KC_BSLASH),KC_Z,KC_X,KC_C,KC_V,KC_COLN,KC_GRAVE,KC_TRANSPARENT,KC_TRANSPARENT,LT(2,KC_SCOLON),LT(1,KC_SLASH),LCTL(LSFT(KC_TAB)),LCTL(KC_TAB),KC_HOME,KC_SPACE,LGUI_T(KC_NO),KC_END,KC_KP_7,KC_8,KC_9,KC_0,KC_MINUS,KC_EQUAL,KC_BSPACE,KC_PASTE,KC_Y,KC_U,KC_I,KC_O,KC_P,KC_BSLASH,KC_H,KC_J,KC_K,KC_L,KC_SCOLON,KC_QUOTE,KC_SLASH,KC_B,KC_N,KC_M,KC_COMMA,KC_DOT,KC_RSHIFT,KC_LPRN,KC_RPRN,KC_LBRACKET,KC_RBRACKET,KC_SLASH,KC_LALT,LCTL_T(KC_ESCAPE),KC_PGUP,KC_PGDOWN,KC_ENTER,LGUI_T(KC_NO)),
 
+  [1] = LAYOUT_ergodox(KC_ESCAPE,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_F6,KC_TRANSPARENT,KC_EXLM,KC_AT,KC_LCBR,KC_RCBR,KC_PIPE,KC_TRANSPARENT,KC_TRANSPARENT,KC_HASH,KC_DLR,KC_LPRN,KC_RPRN,KC_GRAVE,KC_TRANSPARENT,KC_PERC,KC_CIRC,KC_LBRACKET,KC_RBRACKET,KC_TILD,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_F7,KC_F8,KC_F9,KC_F10,KC_F11,KC_F12,KC_DELETE,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_AMPR,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT),
 
-  [1] = LAYOUT_ergodox(
-    // left hand
-    KC_ESC,         KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_F6,          
-    _______,        KC_EXLM,        KC_AT,          KC_LCBR,        KC_RCBR,        KC_PIPE,        _______,        
-    _______,        KC_HASH,        KC_DLR,         KC_LPRN,        KC_RPRN,        KC_GRV,         
-    _______,        KC_PERC,        KC_CIRC,        KC_LBRC,        KC_RBRC,        KC_TILD,        _______,        
-    _______,        _______,        _______,        _______,        _______,        
-                                                                                    _______,        _______,        
-                                                                                                    _______,        
-                                                                    _______,        _______,        _______,        
-        // right hand
-        KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,         KC_F12,         KC_DEL,         
-        _______,        _______,        _______,        _______,        _______,        _______,        _______,        
-                        KC_LEFT,        KC_DOWN,        KC_UP,          KC_RGHT,        _______,        _______,        
-        _______,        KC_AMPR,        _______,        _______,        _______,        _______,        _______,        
-        _______,        _______,        _______,        _______,        _______,        
-        _______,        _______,        
-        _______,        
-        _______,        _______,        _______
-    ),
-
-
-  [2] = LAYOUT_ergodox(
-    // left hand
-    _______,        _______,        _______,        _______,        _______,        _______,        _______,        
-    _______,        _______,        KC_MS_U,        _______,        _______,        _______,        _______,        
-    _______,        KC_MS_L,        KC_MS_D,        KC_MS_R,        _______,        _______,        
-    _______,        _______,        _______,        _______,        _______,        _______,        _______,        
-    _______,        _______,        _______,        _______,        _______,        
-                                                                                    _______,        _______,        
-                                                                                                    _______,        
-                                                                    KC_BTN1,        KC_BTN2,        _______,        
-        // right hand
-        _______,        _______,        _______,        _______,        _______,        _______,        _______,        
-        _______,        _______,        _______,        _______,        _______,        _______,        _______,        
-                        KC_MS_L,        KC_MS_D,        KC_MS_U,        KC_MS_R,        _______,        _______,        
-        _______,        _______,        _______,        _______,        _______,        _______,        _______,        
-        _______,        _______,        _______,        _______,        _______,        
-        _______,        _______,        
-        _______,        
-        _______,        _______,        KC_WBAK
-    ),
-
+  [2] = LAYOUT_ergodox(KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_UP,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_LEFT,KC_MS_DOWN,KC_MS_RIGHT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_BTN1,KC_MS_BTN2,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_LEFT,KC_MS_DOWN,KC_MS_UP,KC_MS_RIGHT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_BTN1,KC_MS_BTN2),
 
 };
 
